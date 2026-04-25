568e847f75020f1a3583421ee15f7a4f23d80cbd
Jose Vivas <jvivasb20@gmail.com>
2026-04-25 11:15:12 -0400

    chore: translate script logs and comments from Spanish to English

--- run_once_install-omz.sh
+++ run_once_install-omz.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Instalar oh-my-zsh si no existe
+# Install oh-my-zsh if not exists
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo "Instalando oh-my-zsh..."
+    echo "Installing oh-my-zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-# Instalar plugins custom
+# Install custom plugins
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-    echo "Instalando zsh-autosuggestions..."
+    echo "Installing zsh-autosuggestions..."
     git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 fi
 
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-    echo "Instalando zsh-syntax-highlighting..."
+    echo "Installing zsh-syntax-highlighting..."
     git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 fi
 
-echo "oh-my-zsh y plugins verificados correctamente."
+echo "oh-my-zsh and plugins verified correctly."
